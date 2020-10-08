e9652291e337ac4a517f18fd9914a5144c6f0022
kawabacchan <daisuke30x@gmail.com>
2020-10-08 15:09:55 +0900

    リスト作成のためのルーティング、コントローラー、ビューの作成

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,22 @@
+class ListsController < ApplicationController
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name, :public_id).merge(user_id: current_user.id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   get 'animes/index'
   root to: "animes#index"
+
+  resources :lists, only: [:new, :create]
 end
