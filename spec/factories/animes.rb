0a02793f7916bd4de13d4f7085a3f1ae45717886
kawabacchan <daisuke30x@gmail.com>
2020-10-09 00:07:47 +0900

    アニメモデルのテストコードの作成

--- spec/factories/animes.rb
+++ spec/factories/animes.rb
@@ -1,5 +1,9 @@
 FactoryBot.define do
   factory :anime do
-    
+    name {Faker::Name.first_name}
+    genre_id {Faker::Number.between(from:2, to:9)}
+    score {Faker::Number.between(from:0, to:100)}
+    memo {Faker::Lorem.sentence}
+    association :list
   end
 end
--- spec/models/anime_spec.rb
+++ spec/models/anime_spec.rb
@@ -1,5 +1,48 @@
 require 'rails_helper'
 
 RSpec.describe Anime, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @anime = FactoryBot.build(:anime)
+  end
+
+  describe "アニメ登録" do
+    context "うまくいく時" do
+      it "name, genre_id, score, memoが全てある時、登録できる" do
+        expect(@anime).to be_valid
+      end
+      it "genre_idが1でも登録できる" do
+        @anime.score = 1
+        expect(@anime).to be_valid
+      end
+      it "scoreがなくても登録できる" do
+        @anime.score = nil
+        expect(@anime).to be_valid
+      end
+      it "memoがなくても登録できる" do
+        @anime.memo = nil
+        expect(@anime).to be_valid
+      end
+    end
+
+    context "うまくいかない時" do
+      it "nameが空の時、登録できない" do
+        @anime.name = nil
+        @anime.valid?
+        expect(@anime.errors.full_messages).to include("Name can't be blank")
+      end
+      it "nameが重複する時、登録できない" do
+        another_anime = FactoryBot.build(:anime)
+        another_anime.save
+        @anime.name = another_anime.name
+        @anime.valid?
+        expect(@anime.errors.full_messages).to include("Name has already been taken")
+      end
+      it "genre_idが空だと登録できない" do
+        @anime.genre_id = nil
+        @anime.valid?
+        expect(@anime.errors.full_messages).to include("Genre can't be blank")
+      end
+
+    end
+  end
 end
