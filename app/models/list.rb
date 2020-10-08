e235db3dbc98ead376603a676cefdc4c67115e61
kawabacchan <daisuke30x@gmail.com>
2020-10-08 14:44:52 +0900

    publicカラムのActiveHashの設定

--- app/models/list.rb
+++ app/models/list.rb
@@ -2,6 +2,10 @@ class List < ApplicationRecord
 
   belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :public
+
   validates :name, presence: true, length: { maximum: 20}
+  validates :public, presence: true
   validates :public_id, numericality: {other_than: 1}
 end
