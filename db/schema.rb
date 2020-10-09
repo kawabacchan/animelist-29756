da7871fcf37f0d0285a283040e107f09d5737c52
kawabacchan <daisuke30x@gmail.com>
2020-10-09 17:49:50 +0900

    フォローモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_09_030804) do
+ActiveRecord::Schema.define(version: 2020_10_09_084503) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2020_10_09_030804) do
     t.index ["list_id"], name: "index_animes_on_list_id"
   end
 
+  create_table "follows", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_follows_on_user_id"
+  end
+
   create_table "lists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "name", null: false
     t.integer "public_id", null: false
@@ -71,5 +78,6 @@ ActiveRecord::Schema.define(version: 2020_10_09_030804) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "animes", "lists"
+  add_foreign_key "follows", "users"
   add_foreign_key "lists", "users"
 end
