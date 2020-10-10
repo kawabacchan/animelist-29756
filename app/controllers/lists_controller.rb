f5798cab86914cdab154ede68f0be51f9be60361
kawabacchan <daisuke30x@gmail.com>
2020-10-10 16:02:00 +0900

    ログイン後のトップ画面の修正とリスト編集機能の作成

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -13,6 +13,25 @@ class ListsController < ApplicationController
     end
   end
 
+  def edit
+    @lists = List.where(user_id: current_user.id)
+    @user = User.find(current_user.id)
+    @follows = Follow.where(user_id: current_user.id)
+    @list = List.find(params[:id])
+  end
+
+  def update
+    @lists = List.where(user_id: current_user.id)
+    @user = User.find(current_user.id)
+    @follows = Follow.where(user_id: current_user.id)
+    @list = List.find(params[:id])
+    if @list.update(list_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
   private
 
   def list_params
